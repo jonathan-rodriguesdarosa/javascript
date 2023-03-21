6f598898d2e4ae9e06f9a8bba048bda3a2942fb8
jonathan-rodriguesdarosa <126508209+jonathan-rodriguesdarosa@users.noreply.github.com>
2023-03-21 16:46:03 -0300

    Aula 11 ex009 JavaScript

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 72
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade máxima permitida, MULTADO!`)
+}
+console.log(`Digira sempre usando cinto do segurança`)
