f3138154d6830a15e3226f0f4a9e8e75cd0fb9c7
jonathan-rodriguesdarosa <126508209+jonathan-rodriguesdarosa@users.noreply.github.com>
2023-03-24 12:38:43 -0300

    Aula 14 Estrutura de Repetição

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -1,5 +1,18 @@
+
+//ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL.
+var c = 1
+do {
+    console.log('Tudo bem?') // (`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+//ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO.
+/*
 var c = 1
 while (c <= 6) {
-    console.log('Tudo bem?')
+    console.log('Tudo bem?') // (`Passo ${c}`)
     c++
 }
+*/
