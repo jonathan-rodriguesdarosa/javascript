4b23acc7e04771795be0b0c3c7dce060bc3edddc
jonathan-rodriguesdarosa <126508209+jonathan-rodriguesdarosa@users.noreply.github.com>
2023-03-27 12:16:26 -0300

    Exercícios de Tabuada

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert ('Olá')
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if(num.value.length == 0 ) {
+        window.alert('Por favor, digite um número')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
+
